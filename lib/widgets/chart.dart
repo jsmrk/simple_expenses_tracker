66e724f3f2cdc9b44ba9ef7a15fe9f8ab9a364e5
Jess Mark Baguio <119231806+jsmrk@users.noreply.github.com>
2023-11-21 01:45:42 +0800

    test

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -35,7 +35,7 @@ class Chart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
-      margin: EdgeInsets.all(20),
+      margin: EdgeInsets.all(21),
       child: Row(
         children: [],
       ),
